78336efc3b96de7fdb7c23a4ae109a8c69c71b58
PiotrK <piotrek380@gmail.com>
2023-02-17 22:07:17 +0100

    Create the _document.tsx file with a lang attribute

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Main, Head, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <div className="container">
+          <Main />
+        </div>
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
